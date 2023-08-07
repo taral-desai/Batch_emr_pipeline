b945cb5ffbaa50da635eac739dee376f2d9180ba
taral-desai <77844108+taral-desai@users.noreply.github.com>
2023-08-06 21:18:04 -0400

    bucket output added in terraform

--- terraform_aws/output.tf
+++ terraform_aws/output.tf
@@ -8,3 +8,8 @@ output "public_key" {
   description = "EC2 public key."
   value       = tls_private_key.rsa_4096_airflow_ec2.public_key_openssh
 }
+
+output "bucket_name" {
+  description = "S3 bucket name."
+  value       = aws_s3_bucket.sde-data-lake.id
+}
